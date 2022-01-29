93b8f11dc90213a0b9abd488b1c9c0999ccd3a7a
Leo <deutschleonardo@gmail.com>
2022-01-29 08:19:04 -0600

    setup App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="main">
+
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App.jsx';
+import App from './App.jsx';
 
-ReactDOM.render(<h1>i</h1>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
