4f28a21b97aae3638526a060e9f663b73c3a4b2e
Winston Pantelakos <winstonspantelakos@gmail.com>
2022-01-29 11:48:21 -0500

    basic nav set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from 'axios';
 import SignIn from './components/SignIn.jsx';
+import NavMenu from './components/NavMenu.jsx';
+// import webMenuIcon from '../assets/menu-48.png';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,13 +15,38 @@ class App extends React.Component {
       showNav: false
     }
     this.captureUser = this.captureUser.bind(this);
+    this.captureNavigation = this.captureNavigation.bind(this);
   }
   captureUser({ name }) {
     this.setState({ username: name });
   }
+  captureNavigation(e) {
+    console.log([e.target.getAttribute('name')]);
+    if (e.target.getAttribute('name') === "showHome") {
+      this.setState({
+        showHome: true,
+        showFavorites: false,
+        showProfile: false
+      });
+    } else if (e.target.getAttribute('name') === "showFavorites") {
+      this.setState({
+        showHome: false,
+        showFavorites: true,
+        showProfile: false
+      });
+    } else if (e.target.getAttribute('name') === "showProfile") {
+      this.setState({
+        showHome: false,
+        showFavorites: false,
+        showProfile: true
+      });
+    }
+  }
   render() {
     return (
       <div className="main">
+        <button onClick={e => this.setState({showNav: !this.state.showNav})}>Menu</button>
+        {this.state.showNav === true ? ( <NavMenu captureNavigation={this.captureNavigation} />) : ''}
         <SignIn captureUser={this.captureUser} />
       </div>
     );
--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,21 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const NavMenu = ({ captureUser }) => {
-  const [user, setUser] = useState({name: '', email: ''});
-  const [error, setError] = useState('');
+const NavMenu = ({ captureNavigation }) => {
 
   return (
-    <>
-      {(user.email !== "") ? (
-        <div className="welcome">
-          <h2>Welcome, <span>{user.name}</span></h2>
-          <button onClick={Logout}>Logout</button>
-        </div>
-      ) : (
-        <LoginForm Login={Login} error={error}/>
-      )}
-    </>
+    <div className="navmenu">
+      <div className="navmenuitem" name="showHome" onClick={e => captureNavigation(e)}>Home</div>
+      <div className="navmenuitem" name="showProfile" onClick={e => captureNavigation(e)}>Profile</div>
+      <div className="navmenuitem" name="showFavorites" onClick={e => captureNavigation(e)}>Favorites</div>
+    </div>
   )
 }
 
