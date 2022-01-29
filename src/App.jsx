f6046963c756395d5a6b4f780c09f33d22fb131d
Winston Pantelakos <winstonspantelakos@gmail.com>
2022-01-29 12:16:30 -0500

    state currentview references modified

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       username: '',
-      showProfile: false,
-      showHome: false,
-      showFavorites: false,
-      showNav: false,
-      showLogin: true
+      currentView: 'login'
     }
     this.captureUser = this.captureUser.bind(this);
     this.captureNavigation = this.captureNavigation.bind(this);
@@ -24,25 +20,7 @@ class App extends React.Component {
     this.setState({ username: name });
   }
   captureNavigation(e) {
-    if (e.target.getAttribute('name') === "showHome") {
-      this.setState({
-        showHome: true,
-        showFavorites: false,
-        showProfile: false
-      });
-    } else if (e.target.getAttribute('name') === "showFavorites") {
-      this.setState({
-        showHome: false,
-        showFavorites: true,
-        showProfile: false
-      });
-    } else if (e.target.getAttribute('name') === "showProfile") {
-      this.setState({
-        showHome: false,
-        showFavorites: false,
-        showProfile: true
-      });
-    }
+      this.setState({ currentView: e.target.getAttribute('name')});
   }
   render() {
     return (
@@ -50,7 +28,7 @@ class App extends React.Component {
         <button onClick={e => this.setState({showNav: !this.state.showNav})}><img src={webMenuIcon}></img></button>
         {this.state.showNav === true ? ( <NavMenu captureNavigation={this.captureNavigation} />) : ''}
 
-        {this.state.showLogin === true ? (<SignIn captureUser={this.captureUser} />) : ''}
+        {this.state.currentView === 'login' ? (<SignIn captureUser={this.captureUser} />) : ''}
 
       </div>
     );
--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -5,9 +5,9 @@ const NavMenu = ({ captureNavigation }) => {
 
   return (
     <div className="navmenu">
-      <div className="navmenuitem" name="showHome" onClick={e => captureNavigation(e)}>Home</div>
-      <div className="navmenuitem" name="showProfile" onClick={e => captureNavigation(e)}>Profile</div>
-      <div className="navmenuitem" name="showFavorites" onClick={e => captureNavigation(e)}>Favorites</div>
+      <div className="navmenuitem" name="home" onClick={e => captureNavigation(e)}>Home</div>
+      <div className="navmenuitem" name="profile" onClick={e => captureNavigation(e)}>Profile</div>
+      <div className="navmenuitem" name="favorites" onClick={e => captureNavigation(e)}>Favorites</div>
     </div>
   )
 }
