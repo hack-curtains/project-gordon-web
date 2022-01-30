d36935b44bdb2d1b4d7dddd742ed08fdca85da61
Winston Pantelakos <winstonspantelakos@gmail.com>
2022-01-30 11:09:01 -0500

    signin reformatted to modal and bottom nav implemented

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+console.log(window.innerWidth);
 const NavMenu = ({ captureNavigation }) => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
 
   return (
-    <div className="navmenu">
-      <div className="navmenuitem" name="home" onClick={e => captureNavigation(e)}>Home</div>
-      <div className="navmenuitem" name="profile" onClick={e => captureNavigation(e)}>Profile</div>
-      <div className="navmenuitem" name="favorites" onClick={e => captureNavigation(e)}>Favorites</div>
-    </div>
+      <div className="navmenu">
+        <div className="navmenuitem" name="home" onClick={e => captureNavigation(e)}>Home</div>
+        <div className="navmenuitem" name="explore" onClick={e => captureNavigation(e)}>Explore</div>
+        <div className="navmenuitem" name="profile" onClick={e => captureNavigation(e)}>Profile</div>
+        <div className="navmenuitem" name="favorites" onClick={e => captureNavigation(e)}>Favorites</div>
+      </div>
   )
 }
 
---
+++ src/components/SignInModal.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const SignInModal = ({ handleClose, showLogin, children, name }) => {
+  const showHideClassName = showLogin ? "modal display" : "modal display-none";
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        {children}
+        <br></br>
+        <div className="closeModal">
+          <br></br>
+
+        </div>
+        <div className="modalClose">
+          <button type="button" name={name} onClick={handleClose}> Close
+
+          </button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SignInModal;
