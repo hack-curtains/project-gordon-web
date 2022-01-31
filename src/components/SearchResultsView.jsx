875d577b2ce540f3a9eaff4a12b68b504162b598
Heine574 <johannes@niemela.se>
2022-01-31 10:38:26 -0600

    Basic functionality for Search View +styles

--- src/components/SearchResultsView.jsx
+++ src/components/SearchResultsView.jsx
@@ -1,13 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { API_ADDR } from '../config';
 
 import SearchView from './SearchView.jsx';
 
-const SearchResultsView = ({ favorites, currentView, captureFavorites, captureNavigation }) => {
+const SearchResultsView = ({ user, favorites, currentView, captureFavorites, captureNavigation }) => {
+
+  const [ingredients, setIngredients] = useState([]);
+  const [ingredientsMap, setIngredientsMap] = useState({});
+  const [pantry, setPantry] = useState({});
+
+  if (ingredients.length === 0) {
+    axios.get(`${API_ADDR}/ingredients`)
+      .then((response) => {
+        setIngredients(response.data);
+        for (let i = 0; i < response.data.length; i++) {
+          let ingredient = response.data[i];
+          ingredientsMap[ingredient.name] = i;
+        }
+        setIngredientsMap({...ingredientsMap});
+      });
+  }
+
+  const togglePantry = (ingredient) => {
+    const newPantry = {...pantry};
+    if (newPantry[ingredient]) {
+      delete newPantry[ingredient];
+    } else {
+      newPantry[ingredient] = true;
+    }
+    setPantry(newPantry);
+  };
 
   return (
     <div>
-      <SearchView />
+      <SearchView ingredients={ingredients} ingredientsMap={ingredientsMap} pantry={pantry} togglePantry={togglePantry} />
     </div>
   )
 }
--- src/components/SearchView.jsx
+++ src/components/SearchView.jsx
@@ -1,21 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { API_ADDR } from '../config';
 
-const SearchView = ({ favorites, currentView, captureFavorites, captureNavigation }) => {
+const SearchView = ({ ingredients, ingredientsMap, pantry, togglePantry }) => {
 
-  const [ingredients, setIngredients] = useState([]);
-  axios.get(`${API_ADDR}/ingredients`)
-    .then((response) => {
-      setIngredients(response);
-    });
+  const [ingredient, setIngredient] = useState('');
+
+  const ingredientDatalist = <datalist id="ingredientsList">
+    {ingredients.map((ingredient, i) => (
+      <option value={ingredient.name} key={i}></option>
+    ))}
+  </datalist>;
+
+  const handleChange = ({ target }) => {
+    setIngredient(target.value);
+  };
+
+  const addIngredient = () => {
+    if (ingredient in ingredientsMap) {
+      if (!pantry[ingredient]) {
+        togglePantry(ingredient);
+        setIngredient('');
+      }
+    }
+  };
 
   return (
-    <div className="SearchView">
-      <div className="PantryContainer">
+    <div className="searchView">
+      <div className="pantryContainer">
         <h1>Pantry</h1>
-        <div className="Pantry">
-          {ingredients.length}
+        <div className="pantry">
+          <div className="addIngredients">
+            <input id="ingredient" list="ingredientsList" value={ingredient} onChange={handleChange}></input>
+            {ingredientDatalist}
+            <button onClick={addIngredient}>Add Ingredient</button>
+          </div>
+          <div>
+            {Object.keys(pantry).map((name, i) => (
+              <p key={i}>{name}</p>
+            ))}
+          </div>
         </div>
       </div>
     </div>
