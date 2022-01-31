0b96e7c109d345d44dda9f52ba5cd1fd68870d1b
Winston Pantelakos <winstonspantelakos@gmail.com>
2022-01-31 17:29:38 -0500

    styling to top nav bar implemented

--- src/components/ProfileView.jsx
+++ src/components/ProfileView.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import profile90 from '../../dist/resources/ProfileView/icons8-user-90.png';
 
-const ProfileView = ({ captureNavigation }) => {
+const ProfileView = ({ captureNavigation, openLogin }) => {
 
   const [user, setUser] = useState({name: 'Winston Pantelakos', email: 'winstonspantelakos@gmail.com'});
+  const [showSignIn, setShowSignIn] = useState(false);
 
   return (
     <div className="profileview">
@@ -12,7 +13,8 @@ const ProfileView = ({ captureNavigation }) => {
       <div className="profileuser">{user.name}</div>
       <div className="profileemail"><strong>Email: </strong>{user.email}</div>
       <button className="profilefavorites" name="favorites" onClick={e => captureNavigation('favorites') }>Favorites</button>
-      <button className="profileSignOut">Sign Out</button>
+      {showSignIn === false ? (<button onClick={e => setShowSignIn(true)} className="profileSignOut">Sign Out</button>) : (<button onClick={e => openLogin()} className="profileSignIn">Sign In</button>) }
+
     </div>
   )
 };
